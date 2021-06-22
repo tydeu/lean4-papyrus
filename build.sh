eeb222f4d139be707a4dc8eda19568d8b21827d9
Mac Malone <tydeu@hatpress.net>
2021-06-22 10:02:10 -0400

    Improve build scripts

--- build.sh
+++ build.sh
@@ -7,11 +7,12 @@ make -C c
 
 LLVM_CONFIG=llvm-config
 LLVM_COMPONENTS="core"
-LEAN_LLVM_LIB="c/build/papyrus.o"
+PAPYRUS_C_LIB="c/build/papyrus.o"
 
 LLVM_LIBDIR=$($LLVM_CONFIG --libdir)
 LLVM_LINK_FLAGS=$($LLVM_CONFIG --ldflags)
 LLVM_LIBS=$($LLVM_CONFIG --link-static --libs $LLVM_COMPONENTS)
 LLVM_SYS_LIBS=$($LLVM_CONFIG --link-static --system-libs)
 
-leanpkg build bin LINK_OPTS="\"$LEAN_LLVM_LIB $LLVM_LINK_FLAGS $LLVM_LIBS $LLVM_SYS_LIBS\""
+rm -rf build/bin
+leanpkg build bin LINK_OPTS="\"$PAPYRUS_C_LIB $LLVM_LINK_FLAGS $LLVM_LIBS $LLVM_SYS_LIBS\""
