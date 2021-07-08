013f85c77b8a1321f9a5e4832d2cdcd454fba82e
tydeu <tydeu@hatpress.net>
2021-07-07 20:49:51 -0400

    refactor: `AddressSpace` is once again a structure

--- Papyrus/IR/AddressSpace.lean
+++ Papyrus/IR/AddressSpace.lean
@@ -1,32 +1,26 @@
 namespace Papyrus
 
-/--
-  A numerically indexed address space.
-  Implemented as a `def` rather than a `structure` so the
-  it will be passed unboxed (as a `uint32`) to external functions.
--/
-def AddressSpace := UInt32
-
-instance : BEq AddressSpace := inferInstanceAs (BEq UInt32)
-instance : DecidableEq AddressSpace := inferInstanceAs (DecidableEq UInt32)
-instance : Repr AddressSpace := inferInstanceAs (Repr UInt32)
+/-- A numerically indexed address space. -/
+structure AddressSpace where
+  index : UInt32
+  deriving BEq, Repr
 
 namespace AddressSpace
 
 /-- The default address space (i.e., 0). -/
-def default : AddressSpace := (0 : UInt32)
+def default := mk 0
 
 /-- Make an address space from a `Nat`. -/
-def ofNat (n : Nat) : AddressSpace  := n.toUInt32
+def ofNat (n : Nat) := mk n.toUInt32
 
 /-- Make an address space from a `UInt32`. -/
-def ofUInt32 (n : UInt32) : AddressSpace := n
+def ofUInt32 (n : UInt32) := mk n
 
 /-- Convert an address space tp a `Nat`. -/
-def toNat (self : AddressSpace) : Nat := UInt32.toNat self
+def toNat (self : AddressSpace) : Nat := self.index.toNat
 
 /-- Convert an address space to a `UInt32`. -/
-def toUInt32 (self : AddressSpace) : UInt32 := self
+def toUInt32 (self : AddressSpace) : UInt32 := self.index
 
 end AddressSpace
 
