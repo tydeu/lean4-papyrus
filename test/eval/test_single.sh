45e56cde9119f97bbfa21512f3a523b31dc466cc
tydeu <tydeu@hatpress.net>
2021-08-20 23:35:43 -0400

    test: use plugin in eval tests

--- test/eval/test_single.sh
+++ test/eval/test_single.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 source ./common.sh
 
+PLUGIN=../../plugin/build/PapyrusPlugin
+
 # these tests don't have to succeed
-exec_capture lean -DprintMessageEndPos=true "$f" || true
+exec_capture lean --plugin ${PLUGIN} -DprintMessageEndPos=true "$f" || true
 diff_produced
