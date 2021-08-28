e9faffd97d65a87b064773c2bf49e75442fc9f5a
tydeu <tydeu@hatpress.net>
2021-08-28 18:08:48 -0400

    test: add `lean_hello` jit test

--- test/out/script/jit.lean
+++ test/out/script/jit.lean
@@ -18,3 +18,13 @@ llvm module empty do
   pure ()
 
 #jit empty -- Error: Module has no main function
+
+llvm module lean_hello do
+  declare %lean_object* @lean_mk_string(i8*)
+  declare %lean_object* @l_IO_println___at_Lean_instEval___spec__1(%lean_object*, %lean_object*);
+  define i32 @main() do
+   %hello = call @lean_mk_string("Hello World!"*)
+   call @l_IO_println___at_Lean_instEval___spec__1(%hello, %hello)
+   ret i32 0
+
+#jit lean_hello
