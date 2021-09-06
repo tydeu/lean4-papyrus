a58df842e3ae03ac6a9eff2d48eb1e56d42d66f7
tydeu <tydeu@hatpress.net>
2021-09-05 23:32:17 -0400

    refactor: cleanup `ExectionEngineRef`  formatting

--- Papyrus/ExecutionEngineRef.lean
+++ Papyrus/ExecutionEngineRef.lean
@@ -6,57 +6,57 @@ namespace Papyrus
 
 /-- A desired execution engine kind. -/
 inductive EngineKind
-| Either
-| JIT
-| Interpreter
-deriving BEq, Repr
+| either
+| jit
+| interpreter
+deriving BEq, DecidableEq, Repr
 
 attribute [unbox] EngineKind
-instance : Inhabited EngineKind := ⟨EngineKind.Either⟩
+instance : Inhabited EngineKind := ⟨EngineKind.either⟩
 
 namespace EngineKind
 
-def isJIT (self : EngineKind) : Bool :=
-  self matches JIT
+def isJit (self : EngineKind) : Bool :=
+  self matches jit
 
-def allowsJIT : (self : EngineKind) → Bool
-| JIT => true
-| Interpreter => false
-| Either => true
+def allowsJit : (self : EngineKind) → Bool
+| jit => true
+| interpreter => false
+| either => true
 
 def isInterpreter (self : EngineKind) : Bool :=
-  self matches Interpreter
+  self matches interpreter
 
 def allowsInterpreter : (self : EngineKind) → Bool
-| JIT => false
-| Interpreter => true
-| Either => true
+| jit => false
+| interpreter => true
+| either => true
 
 def isEither (self : EngineKind) : Bool :=
-  self matches Either
+  self matches either
 
 def ofUInt8 (value : UInt8) : EngineKind :=
-  if value == 0b01 then JIT else
-  if value == 0b10 then Interpreter else
-  Either
+  if value == 0b01 then jit else
+  if value == 0b10 then interpreter else
+  either
 
 def toUInt8 : (self : EngineKind) → UInt8
-| JIT => 0b01
-| Interpreter => 0b10
-| Either => 0b11
+| jit => 0b01
+| interpreter => 0b10
+| either => 0b11
 
 end EngineKind
 
 /-- Optimization level for generated code. -/
 inductive OptLevel
-| /-- -O0 -/ None
-| /-- -O1 -/ Less
-| /-- -O2 -/ Default
-| /-- -O3 -/ Aggressive
-deriving BEq, Repr
+| /-- -O0 -/ none
+| /-- -O1 -/ less
+| /-- -O2 -/ default
+| /-- -O3 -/ aggressive
+deriving BEq, DecidableEq, Repr
 
 attribute [unbox] OptLevel
-instance : Inhabited OptLevel := ⟨OptLevel.Default⟩
+instance : Inhabited OptLevel := ⟨OptLevel.default⟩
 
 /--
   A reference to an external LLVM
@@ -68,27 +68,26 @@ namespace ExecutionEngineRef
 
 /-- Create an execution engine for the given module. -/
 @[extern "papyrus_execution_engine_create_for_module"]
-constant createForModule (mod : @& ModuleRef) (kind : @& EngineKind := EngineKind.Either)
+constant createForModule (mod : @& ModuleRef) (kind : @& EngineKind := EngineKind.either)
   (march : @& String := "") (mcpu : @& String := "") (mattrs : @& Array String := #[])
-  (optLevel : @& OptLevel := OptLevel.Default) (verifyModule := false)
+  (optLevel : @& OptLevel := OptLevel.default) (verifyModule := false)
   : IO ExecutionEngineRef
 
 /--
   Execute the given function with the given arguments, and return the result.
 
-  MCJIT execution engines can only execute 'main-like' function.
+  An MCJIT execution engine can only execute 'main-like' function.
   That is, those returning `void` or `int` and taking no arguments
-  (i.e., `[]`) or `argc`/`argv` (i.e., `[int, char**]`).
+  (i.e., `[]`) or `argc`/`argv` (i.e., `[i32, i8**]`).
 -/
 @[extern "papyrus_execution_engine_run_function"]
 constant runFunction (fn : @& FunctionRef) (self : @& ExecutionEngineRef)
   (args : @& Array GenericValueRef := #[]) : IO GenericValueRef
 
 /--
-  A helper function to wrap the behavior of `runFunction`
-  to handle common task of starting up a `main` function that may take
-  up to three arguments (`i32 argc`, `i8** argv`, and `i8** envp`) and
-  return an `i32` exit code.
+  A helper for `runFunction` that runs a standard  `main`-like function.
+  That is, a function that may take up to three arguments  (`i32 argc`,
+  `i8** argv`, and `i8** envp`) and return a `i32` exit code.
 -/
 @[extern "papyrus_execution_engine_run_function_as_main"]
 constant runFunctionAsMain (fn : @& FunctionRef) (self : @& ExecutionEngineRef)
