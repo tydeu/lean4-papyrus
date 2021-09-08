cd184d7b4620b42b8aa664579e060390830d080e
tydeu <tydeu@hatpress.net>
2021-09-07 20:05:32 -0400

    refactor: reorganize instructions to follow opcode order

--- Papyrus/IR/InstructionRefs.lean
+++ Papyrus/IR/InstructionRefs.lean
@@ -8,6 +8,52 @@ import Papyrus.IR.FunctionRef
 namespace Papyrus
 
 --------------------------------------------------------------------------------
+-- # Return Instructions
+--------------------------------------------------------------------------------
+
+/--
+  A reference to an external LLVM
+  [ReturnInst](https://llvm.org/doxygen/classllvm_1_1ReturnInst.html).
+-/
+def ReturnInstRef := InstructionRef
+
+namespace ReturnInstRef
+
+/-- Create a new unlinked void return instruction. -/
+@[extern "papyrus_return_inst_create_void"]
+constant createVoid : LlvmM ReturnInstRef
+
+/-- Create a new unlinked return instruction. -/
+@[extern "papyrus_return_inst_create"]
+constant create (retVal : @& ValueRef) : LlvmM ReturnInstRef
+
+/-- Create a new unlinked i1 return instruction. -/
+def createBool (retVal : Bool) : LlvmM ReturnInstRef := do
+  create (← ConstantIntRef.ofBool retVal)
+
+/-- Create a new unlinked i8 return instruction. -/
+def createUInt8 (retVal : UInt8) : LlvmM ReturnInstRef := do
+  create (← ConstantIntRef.ofUInt8 retVal)
+
+/-- Create a new unlinked i16 return instruction. -/
+def createUInt16 (retVal : UInt16) : LlvmM ReturnInstRef := do
+  create (← ConstantIntRef.ofUInt16 retVal)
+
+/-- Create a new unlinked i32 return instruction. -/
+def createUInt32 (retVal : UInt32) : LlvmM ReturnInstRef := do
+  create (← ConstantIntRef.ofUInt32 retVal)
+
+/-- Create a new unlinked i64 return instruction. -/
+def createUInt64 (retVal : UInt64) : LlvmM ReturnInstRef := do
+  create (← ConstantIntRef.ofUInt64 retVal)
+
+/-- Get a reference to the returned value. -/
+@[extern "papyrus_return_inst_get_value"]
+constant getReturnValue (self : @& ReturnInstRef) : IO (Option ValueRef)
+
+end ReturnInstRef
+
+--------------------------------------------------------------------------------
 -- # GetElementPtr Instructions
 --------------------------------------------------------------------------------
 
@@ -91,49 +137,3 @@ def createCall
   CallInstRef.create (← self.getType) self args name
 
 end FunctionRef
-
---------------------------------------------------------------------------------
--- # Return Instructions
---------------------------------------------------------------------------------
-
-/--
-  A reference to an external LLVM
-  [ReturnInst](https://llvm.org/doxygen/classllvm_1_1ReturnInst.html).
--/
-def ReturnInstRef := InstructionRef
-
-namespace ReturnInstRef
-
-/-- Create a new unlinked void return instruction. -/
-@[extern "papyrus_return_inst_create_void"]
-constant createVoid : LlvmM ReturnInstRef
-
-/-- Create a new unlinked return instruction. -/
-@[extern "papyrus_return_inst_create"]
-constant create (retVal : @& ValueRef) : LlvmM ReturnInstRef
-
-/-- Create a new unlinked i1 return instruction. -/
-def createBool (retVal : Bool) : LlvmM ReturnInstRef := do
-  create (← ConstantIntRef.ofBool retVal)
-
-/-- Create a new unlinked i8 return instruction. -/
-def createUInt8 (retVal : UInt8) : LlvmM ReturnInstRef := do
-  create (← ConstantIntRef.ofUInt8 retVal)
-
-/-- Create a new unlinked i16 return instruction. -/
-def createUInt16 (retVal : UInt16) : LlvmM ReturnInstRef := do
-  create (← ConstantIntRef.ofUInt16 retVal)
-
-/-- Create a new unlinked i32 return instruction. -/
-def createUInt32 (retVal : UInt32) : LlvmM ReturnInstRef := do
-  create (← ConstantIntRef.ofUInt32 retVal)
-
-/-- Create a new unlinked i64 return instruction. -/
-def createUInt64 (retVal : UInt64) : LlvmM ReturnInstRef := do
-  create (← ConstantIntRef.ofUInt64 retVal)
-
-/-- Get a reference to the returned value. -/
-@[extern "papyrus_return_inst_get_value"]
-constant getReturnValue (self : @& ReturnInstRef) : IO (Option ValueRef)
-
-end ReturnInstRef
--- c/src/instruction.cpp
+++ c/src/instruction.cpp
@@ -15,6 +15,37 @@ llvm::Instruction* toInstruction(lean::object* instRef) {
 }
 
 //------------------------------------------------------------------------------
+// Return instructions
+//------------------------------------------------------------------------------
+
+// Get the LLVM ReturnInst pointer wrapped in an object.
+ReturnInst* toReturnInst(lean::object* instRef) {
+	return llvm::cast<ReturnInst>(toValue(instRef));
+}
+
+// Get a reference to a newly created return instruction.
+extern "C" obj_res papyrus_return_inst_create
+(b_obj_arg retValObj, obj_arg ctxRef, obj_arg /* w */)
+{
+	auto inst = ReturnInst::Create(*toLLVMContext(ctxRef), toValue(retValObj));
+	return io_result_mk_ok(mkValueRef(ctxRef, inst));
+}
+
+// Get a reference to a newly created void return instruction.
+extern "C" obj_res papyrus_return_inst_create_void(obj_arg ctxRef, obj_arg /* w */) {
+	auto inst = ReturnInst::Create(*toLLVMContext(ctxRef));
+	return io_result_mk_ok(mkValueRef(ctxRef, inst));
+}
+
+// Get a reference to the value returned by the instruction.
+extern "C" obj_res papyrus_return_inst_get_value(b_obj_arg instObj, obj_arg /* w */) {
+	auto value = toReturnInst(instObj)->getReturnValue();
+	auto obj = value == nullptr ? mk_option_none() :
+		mk_option_some(mkValueRef(getValueContext(instObj), value));
+	return io_result_mk_ok(obj);
+}
+
+//------------------------------------------------------------------------------
 // GetElementPtr instructions
 //------------------------------------------------------------------------------
 
@@ -95,37 +126,6 @@ extern "C" obj_res papyrus_call_inst_create
 	auto i = CallInst::Create(toFunctionType(typeRef), toValue(funVal), args,
 		refOfString(nameObj));
 	return io_result_mk_ok(mkValueRef(copyLink(typeRef), i));
-}
-
-//------------------------------------------------------------------------------
-// Return instructions
-//------------------------------------------------------------------------------
-
-// Get the LLVM ReturnInst pointer wrapped in an object.
-ReturnInst* toReturnInst(lean::object* instRef) {
-	return llvm::cast<ReturnInst>(toValue(instRef));
-}
-
-// Get a reference to a newly created return instruction.
-extern "C" obj_res papyrus_return_inst_create
-(b_obj_arg retValObj, obj_arg ctxRef, obj_arg /* w */)
-{
-	auto inst = ReturnInst::Create(*toLLVMContext(ctxRef), toValue(retValObj));
-	return io_result_mk_ok(mkValueRef(ctxRef, inst));
-}
-
-// Get a reference to a newly created void return instruction.
-extern "C" obj_res papyrus_return_inst_create_void(obj_arg ctxRef, obj_arg /* w */) {
-	auto inst = ReturnInst::Create(*toLLVMContext(ctxRef));
-	return io_result_mk_ok(mkValueRef(ctxRef, inst));
-}
-
-// Get a reference to the value returned by the instruction.
-extern "C" obj_res papyrus_return_inst_get_value(b_obj_arg instObj, obj_arg /* w */) {
-	auto value = toReturnInst(instObj)->getReturnValue();
-	auto obj = value == nullptr ? mk_option_none() :
-		mk_option_some(mkValueRef(getValueContext(instObj), value));
-	return io_result_mk_ok(obj);
 }
 
 } // end namespace papyrus
