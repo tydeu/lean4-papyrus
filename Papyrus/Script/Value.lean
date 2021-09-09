1c2650895a816f4334aced5da2061bdd3d17b2de
tydeu <tydeu@hatpress.net>
2021-09-09 19:58:14 -0400

    featL add `ptrtoint`/`inttoptr` constants to DSL + partial test

--- Papyrus/Script/Value.lean
+++ Papyrus/Script/Value.lean
@@ -1,7 +1,7 @@
 import Lean.Parser
+import Papyrus.Script.Type
 import Papyrus.Script.ParserUtil
 import Papyrus.Script.AddressSpace
-import Papyrus.Script.IntegerType
 import Papyrus.IR.ConstantRefs
 import Papyrus.Builders
 
@@ -30,7 +30,31 @@ macro "%" x:ident : llvmValue => x
 macro "value" "(" x:term ")" : llvmValue => x
 macro (priority := low) x:ident : llvmValue => x
 
--- # Integer Constants
+--------------------------------------------------------------------------------
+-- # Constants
+--------------------------------------------------------------------------------
+
+-- ## Category
+
+declare_syntax_cat llvmConst (behavior := symbol)
+def constParser (rbp : Nat := 0) := categoryParser `llvmConst rbp
+
+-- ## Expansion
+
+def expandConstantAsRef (stx : Syntax) : MacroM Syntax :=
+  expandMacros stx
+
+def expandConstantAsRefArrow (stx : Syntax) : MacroM Syntax := do
+  `(← $(← expandConstantAsRef stx))
+
+macro c:llvmConst : llvmValue => expandConstantAsRef c
+
+-- ## Constant Global String Pointers
+
+macro s:strLit addrspace?:optional(addrspace) "*" : llvmConst  => do
+  ``(stringPtr $s $(← expandOptAddrspace addrspace?))
+
+-- ## Integer Constants
 
 @[runParserAttributeHooks]
 def constIntLit := leading_parser
@@ -43,14 +67,39 @@ def expandConstIntLitAsRef : Macro
   ``(ConstantIntRef.ofInt $(← expandIntTypeLitAsNatLit t) $(← expandNegNumLit n))
 | stx => Macro.throwErrorAt stx "ill-formed constant int literal"
 
-macro x:constIntLit : llvmValue => expandConstIntLitAsRef x
+macro x:constIntLit : llvmConst => expandConstIntLitAsRef x
 
--- # Boolean Constants
+-- ## Boolean Constants
 
-macro x:"true"  : llvmValue => mkCIdentFrom x ``ConstantIntRef.getTrue
-macro x:"false" : llvmValue => mkCIdentFrom x ``ConstantIntRef.getFalse
+macro x:"true"  : llvmConst => mkCIdentFrom x ``ConstantIntRef.getTrue
+macro x:"false" : llvmConst => mkCIdentFrom x ``ConstantIntRef.getFalse
 
--- # String Pointers
+-- ## Constant Expressions
 
-macro s:strLit addrspace?:optional(addrspace) "*" : llvmValue => do
-  ``(stringPtr $s $(← expandOptAddrspace addrspace?))
+@[runParserAttributeHooks]
+def constPtrToInt := leading_parser
+  nonReservedSymbol "ptrtoint " true >>
+  "(" >> constParser >> nonReservedSymbol " to " true >> typeParser >> ")"
+
+def expandConstPtrToIntAsRef : Macro
+| `(constPtrToInt| ptrtoint ($c:llvmConst to $ty:llvmType)) => do
+  let ty ← expandTypeAsRefArrow ty
+  let c ← expandConstantAsRefArrow c
+  ``(ConstantExprRef.getPtrToInt $c $ty)
+| stx => Macro.throwErrorAt stx "ill-formed constant ptrtoint expression"
+
+macro x:constPtrToInt : llvmConst => expandConstPtrToIntAsRef x
+
+@[runParserAttributeHooks]
+def constIntToPtr := leading_parser
+   nonReservedSymbol "inttoptr " true >>
+  "(" >> constParser >> nonReservedSymbol " to " true >> typeParser >> ")"
+
+def expandConstIntToPtrAsRef : Macro
+| `(constIntToPtr| inttoptr ($c:llvmConst to $ty:llvmType)) => do
+  let ty ← expandTypeAsRefArrow ty
+  let c ← expandConstantAsRefArrow c
+  ``(ConstantExprRef.getIntToPtr $c $ty)
+| stx => Macro.throwErrorAt stx "ill-formed constant inttoptr expression"
+
+macro x:constIntToPtr : llvmConst => expandConstIntToPtrAsRef x
--- test/out/script/program.lean
+++ test/out/script/program.lean
@@ -7,7 +7,7 @@ llvm module lean_hello do
   declare %lean_object* @l_IO_println___at_Lean_instEval___spec__1(%lean_object*, %lean_object*)
   define i32 @main() do
    %hello = call @lean_mk_string("Hello World!"*)
-   call @l_IO_println___at_Lean_instEval___spec__1(%hello, %hello)
+   call @l_IO_println___at_Lean_instEval___spec__1(%hello, inttoptr (i32 1 to %lean_object*))
    ret i32 0
 
 #dump lean_hello
@@ -21,7 +21,7 @@ llvm module lean_echo do
     %arg1p = getelementptr i8*, %argv, i32 1
     %arg1 = load i8*, %arg1p
     %arg1s = call @lean_mk_string(%arg1)
-    call @l_IO_println___at_Lean_instEval___spec__1(%arg1s, %arg1s)
+    call @l_IO_println___at_Lean_instEval___spec__1(%arg1s, inttoptr (i32 1 to %lean_object*))
     ret i32 0
 
 #dump lean_echo
@@ -32,17 +32,18 @@ llvm module lean_select do
   declare %lean_object* @lean_mk_string(i8*)
   declare %lean_object* @l_IO_println___at_Lean_instEval___spec__1(%lean_object*, %lean_object*)
   define i32 @main(i32 %argc, i8** %argv) do
+    let realW ← llvm inttoptr (i32 1 to %lean_object*)
     br1:
       %arg1p = getelementptr i8*, %argv, i32 1
       %arg1 = load i8*, %arg1p
       %arg1s = call @lean_mk_string(%arg1)
-      call @l_IO_println___at_Lean_instEval___spec__1(%arg1s, %arg1s)
+      call @l_IO_println___at_Lean_instEval___spec__1(%arg1s, realW)
       ret i32 0
     br2:
       %arg2p = getelementptr i8*, %argv, i32 2
       %arg2 = load i8*, %arg2p
       %arg2s = call @lean_mk_string(%arg2)
-      call @l_IO_println___at_Lean_instEval___spec__1(%arg2s, %arg2s)
+      call @l_IO_println___at_Lean_instEval___spec__1(%arg2s, realW)
       ret i32 0;
     %arg0 = load i8*, %argv
     %arg0c = load i1, %arg0
